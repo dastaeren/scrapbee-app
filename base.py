f740fcdf6c9b8a9d857f65ef5aa7dbf52bdc9750
Deki Yangzom <141300974+dastaeren@users.noreply.github.com>
2026-02-24 11:35:46 +0600

    Delete base.py

--- base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from core.models import ColumnSchema, Settings, ScrapeResult
-
-
-class BaseScraper(ABC):
-    @abstractmethod
-    def scrape_one(self, url_or_query: str, schema: ColumnSchema, settings: Settings) -> ScrapeResult:
-        raise NotImplementedError
